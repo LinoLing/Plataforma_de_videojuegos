1d59c1244e8511e30b6ad698dbfe1a7365341f2a
LinoLing <1640107@senati.pe>
2025-10-22 08:02:13 -0500

    Feature: agregado en el index

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './swagger.js'; 
 
 dotenv.config();
 
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) =>{
